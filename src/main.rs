6cdb7aeef165a06892bc7d47496f4d174f91109d
Nicolas Rio <nicolas.rio42@gmail.com>
2024-07-01 17:44:23 +0200

    Vegetation works

--- src/main.rs
+++ src/main.rs
@@ -96,9 +96,6 @@ fn main() {
     //     handle.join().unwrap();
     // }
 
-    let duration = start.elapsed();
-    println!("Tiles generated in {:.1?}", duration);
-
     generate_png_from_dem_vegetation_density_tiff_images_and_vector_file(
         get_tile_struct_from_lidar_hd_tile_filename(&tile),
         NeighborTiles {
@@ -113,6 +110,8 @@ fn main() {
         },
     );
 
+    let duration = start.elapsed();
+    println!("Tiles generated in {:.1?}", duration);
     // let tile_list = get_tile_list_from_extent(616112, 6163204, 616692, 6163693);
 
     // // TODO: multithreading
@@ -164,39 +163,39 @@ fn generate_png_from_dem_vegetation_density_tiff_images_and_vector_file(
     );
 }
 
-fn process_sigle_tile(min_x: u64, min_y: u64, max_x: u64, max_y: u64, skip_lidar: bool) {
-    let buffer = 200;
-    let out_dir = Path::new("out").join(format!("{:0>7}_{:0>7}", min_x, max_y));
+// fn process_sigle_tile(min_x: u64, min_y: u64, max_x: u64, max_y: u64, skip_lidar: bool) {
+//     let buffer = 200;
+//     let out_dir = Path::new("out").join(format!("{:0>7}_{:0>7}", min_x, max_y));
 
-    if !skip_lidar {
-        delete_dir_contents(fs::read_dir(&out_dir));
-        create_dir_all(&out_dir).expect("Could not create out dir");
-        generate_pipeline_for_single_tile(min_x, min_y, max_x, max_y, buffer, &out_dir);
-        process_lidar(&out_dir)
-    } else {
-        println!("Skipping LiDAR processing.");
-    }
+//     if !skip_lidar {
+//         delete_dir_contents(fs::read_dir(&out_dir));
+//         create_dir_all(&out_dir).expect("Could not create out dir");
+//         generate_pipeline_for_single_tile(min_x, min_y, max_x, max_y, buffer, &out_dir);
+//         process_lidar(&out_dir)
+//     } else {
+//         println!("Skipping LiDAR processing.");
+//     }
 
-    let config = get_config();
+//     let config = get_config();
 
-    let image_width = ((max_x - min_x) as f32 * config.dpi_resolution / INCH) as u32;
-    let image_height = ((max_y - min_y) as f32 * config.dpi_resolution / INCH) as u32;
+//     let image_width = ((max_x - min_x) as f32 * config.dpi_resolution / INCH) as u32;
+//     let image_height = ((max_y - min_y) as f32 * config.dpi_resolution / INCH) as u32;
 
-    render_vegetation(image_width, image_height, buffer, &config, &out_dir);
-    render_cliffs(image_width, image_height, buffer, &config, &out_dir);
-    render_contours_to_png(image_width, image_height, &config, min_x, min_y, &out_dir);
+//     render_vegetation(image_width, image_height, buffer, &config, &out_dir);
+//     render_cliffs(image_width, image_height, buffer, &config, &out_dir);
+//     render_contours_to_png(image_width, image_height, &config, min_x, min_y, &out_dir);
 
-    let osm_path = Path::new("in").join(format!("{:0>7}_{:0>7}.osm", min_x, max_y));
+//     let osm_path = Path::new("in").join(format!("{:0>7}_{:0>7}.osm", min_x, max_y));
 
-    render_vector_shapes(
-        image_width,
-        image_height,
-        &config,
-        min_x,
-        min_y,
-        &out_dir,
-        &osm_path,
-    );
+//     render_vector_shapes(
+//         image_width,
+//         image_height,
+//         &config,
+//         min_x,
+//         min_y,
+//         &out_dir,
+//         &osm_path,
+//     );
 
-    render_full_map_to_png(image_width, image_height, &out_dir);
-}
+//     render_full_map_to_png(image_width, image_height, &out_dir);
+// }
--- src/vegetation.rs
+++ src/vegetation.rs
@@ -13,7 +13,7 @@ pub fn render_vegetation(
     neighbor_tiles: NeighborTiles,
     image_width: u32,
     image_height: u32,
-    buffer: u64,
+    buffer: usize,
     config: &Config,
 ) {
     println!("Rendering vegetation");
@@ -23,123 +23,124 @@ pub fn render_vegetation(
 
     let casted_vegetation_block_size_pixel = vegetation_block_size_pixel.ceil() as u32;
 
-    let medium_vegetation_path = tile.dir_path.join("medium-vegetation.tif");
-    let high_vegetation_path = tile.dir_path.join("high-vegetation.tif");
+    let high_vegetation_pixels_with_buffer =
+        get_pixels_with_buffer(&tile, &neighbor_tiles, buffer, "high-vegetation.tif");
 
-    let forest_density_tif_file =
-        File::open(high_vegetation_path).expect("Cannot find high vegetation tif image!");
+    let medium_vegetation_pixels_with_buffer =
+        get_pixels_with_buffer(&tile, &neighbor_tiles, buffer, "medium-vegetation.tif");
 
-    let mut forest_img_decoder =
-        Decoder::new(forest_density_tif_file).expect("Cannot create decoder");
-
-    forest_img_decoder = forest_img_decoder.with_limits(tiff::decoder::Limits::unlimited());
-    let forest_width = forest_img_decoder.dimensions().unwrap().0;
     let mut vegetation_layer_img = RgbaImage::from_pixel(image_width, image_height, WHITE);
 
-    let DecodingResult::F64(image_data) = forest_img_decoder.read_image().unwrap() else {
-        panic!("Cannot read band data")
-    };
+    for x_index in buffer..((tile.max_x + buffer as i64 - tile.min_x) as usize) {
+        for y_index in buffer..((tile.max_y + buffer as i64 - tile.min_y) as usize) {
+            let x_pixel = ((x_index - buffer) as f32 * vegetation_block_size_pixel) as i32;
+            let y_pixel = ((y_index - buffer) as f32 * vegetation_block_size_pixel) as i32;
 
-    for index in 0..image_data.len() {
-        let x = index % usize::try_from(forest_width).unwrap();
-        let y = index / usize::try_from(forest_width).unwrap();
+            let high_vegetation_density =
+                get_average_pixel_value(&high_vegetation_pixels_with_buffer, x_index, y_index, 3);
 
-        let x_pixel = ((x as i64 - buffer as i64) as f32 * vegetation_block_size_pixel) as i32;
-        let y_pixel = ((y as i64 - buffer as i64) as f32 * vegetation_block_size_pixel) as i32;
-
-        if x_pixel < 0
-            || x_pixel > image_width as i32
-            || y_pixel < 0
-            || y_pixel > image_height as i32
-        {
-            continue;
-        }
-
-        let forest_density = image_data[index];
-
-        if forest_density > config.yellow_threshold {
-            continue;
-        }
-
-        draw_filled_rect_mut(
-            &mut vegetation_layer_img,
-            Rect::at(x_pixel, y_pixel).of_size(
-                casted_vegetation_block_size_pixel,
-                casted_vegetation_block_size_pixel,
-            ),
-            YELLOW,
-        );
-    }
-
-    let green_density_tif_file =
-        File::open(medium_vegetation_path).expect("Cannot find medium vegetation tif image!");
-
-    let mut green_img_decoder =
-        Decoder::new(green_density_tif_file).expect("Cannot create decoder");
-
-    green_img_decoder = green_img_decoder.with_limits(tiff::decoder::Limits::unlimited());
-    let (green_width, _) = green_img_decoder.dimensions().unwrap();
-
-    let DecodingResult::F64(image_data) = green_img_decoder.read_image().unwrap() else {
-        panic!("Cannot read band data")
-    };
-
-    // Todo: group the two for loops into one
-    for index in 0..image_data.len() {
-        let x = index % usize::try_from(green_width).unwrap();
-        let y = index / usize::try_from(green_width).unwrap();
-
-        let x_pixel = ((x as i64 - buffer as i64) as f32 * vegetation_block_size_pixel) as i32;
-        let y_pixel = ((y as i64 - buffer as i64) as f32 * vegetation_block_size_pixel) as i32;
-
-        if x_pixel < 0
-            || x_pixel > image_width as i32
-            || y_pixel < 0
-            || y_pixel > image_height as i32
-        {
-            continue;
-        }
-
-        let green_density = image_data[index];
-
-        let mut green_color: Option<Rgba<u8>> = None;
-
-        if green_density > config.green_3_threshold {
-            green_color = Some(GREEN_3);
-        } else if green_density > config.green_2_threshold {
-            green_color = Some(GREEN_2);
-        } else if green_density > config.green_1_threshold {
-            green_color = Some(GREEN_1);
-        }
-
-        match green_color {
-            Some(color) => {
+            if high_vegetation_density < config.yellow_threshold {
                 draw_filled_rect_mut(
                     &mut vegetation_layer_img,
                     Rect::at(x_pixel, y_pixel).of_size(
                         casted_vegetation_block_size_pixel,
                         casted_vegetation_block_size_pixel,
                     ),
-                    color,
+                    YELLOW,
                 );
             }
-            _ => (),
+
+            let medium_vegetation_density =
+                get_average_pixel_value(&medium_vegetation_pixels_with_buffer, x_index, y_index, 3);
+
+            let mut green_color: Option<Rgba<u8>> = None;
+
+            // println!("{}", medium_vegetation_density);
+
+            if medium_vegetation_density > config.green_3_threshold {
+                green_color = Some(GREEN_3);
+            } else if medium_vegetation_density > config.green_2_threshold {
+                green_color = Some(GREEN_2);
+            } else if medium_vegetation_density > config.green_1_threshold {
+                green_color = Some(GREEN_1);
+            }
+
+            match green_color {
+                Some(color) => {
+                    draw_filled_rect_mut(
+                        &mut vegetation_layer_img,
+                        Rect::at(x_pixel, y_pixel).of_size(
+                            casted_vegetation_block_size_pixel,
+                            casted_vegetation_block_size_pixel,
+                        ),
+                        color,
+                    );
+                }
+                _ => (),
+            }
         }
     }
 
-    let vegetation_output_path = out_dir.join("vegetation.png");
+    let vegetation_output_path = tile.dir_path.join("vegetation.png");
 
     vegetation_layer_img
         .save(vegetation_output_path)
         .expect("could not save output png");
 }
 
+fn get_average_pixel_value(
+    pixels: &Vec<Vec<f64>>,
+    x_index: usize,
+    y_index: usize,
+    distance: usize,
+) -> f64 {
+    // TODO: fix this naive averaging function
+    let mut count = 0.0;
+    let mut sum = 0.0;
+
+    if pixels.len() == 0 {
+        panic!("Image with null width")
+    }
+
+    let pixels_y_len = pixels[0].len() - 1;
+
+    let min_x = if distance > x_index {
+        0
+    } else {
+        x_index - distance
+    };
+    let max_x = if x_index + distance > pixels.len() {
+        pixels.len()
+    } else {
+        x_index + distance + 1
+    };
+    let min_y = if distance > y_index {
+        0
+    } else {
+        y_index - distance
+    };
+    let max_y = if y_index + distance > pixels_y_len {
+        pixels_y_len
+    } else {
+        y_index + distance + 1
+    };
+
+    for x in min_x..max_x {
+        for y in min_y..max_y {
+            count += 1.0;
+            sum += pixels[x][y];
+        }
+    }
+
+    return sum / count;
+}
+
 fn get_pixels_with_buffer(
-    tile: Tile,
-    neighbor_tiles: NeighborTiles,
+    tile: &Tile,
+    neighbor_tiles: &NeighborTiles,
     buffer_pixels: usize,
     tif_filename: &str,
-) {
+) -> Vec<Vec<f64>> {
     // Because 1pixel = 1 metter
     let tile_width_pixels = (tile.max_x - tile.min_x) as usize;
     let tile_height_pixels = (tile.max_y - tile.min_y) as usize;
@@ -150,10 +151,89 @@ fn get_pixels_with_buffer(
         buffer_pixels * 2 + tile_height_pixels
     ];
 
+    let tile_image_data = get_image_data_from_tif(&tile.dir_path.join(tif_filename));
+
+    for index in 0..tile_image_data.len() {
+        let y = index / tile_width_pixels;
+        let x = index % tile_width_pixels;
+
+        pixels[buffer_pixels + x][buffer_pixels + y] = tile_image_data[index];
+    }
+
     // TODO: find a way to skip iteration of out of bound points if perf issues
     if neighbor_tiles.top.is_some() {
-        let neighbor_tile = neighbor_tiles.top.unwrap();
-        // Because 1pixel = 1 metter
+        let neighbor_tile = neighbor_tiles.top.as_ref().unwrap();
+        let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
+        let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
+
+        for index in 0..image_data.len() {
+            let y = index / neighbor_tile_width_pixels;
+
+            if y > buffer_pixels - 1 {
+                continue;
+            }
+
+            let x = index % neighbor_tile_width_pixels;
+            pixels[buffer_pixels + x][buffer_pixels + tile_height_pixels + y] = image_data[index];
+        }
+    }
+
+    if neighbor_tiles.top_right.is_some() {
+        let neighbor_tile = neighbor_tiles.top_right.as_ref().unwrap();
+        let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
+        let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
+
+        for index in 0..image_data.len() {
+            let x = index % neighbor_tile_width_pixels;
+            let y = index / neighbor_tile_width_pixels;
+
+            if y > buffer_pixels - 1 || x > buffer_pixels - 1 {
+                continue;
+            }
+
+            pixels[buffer_pixels + tile_width_pixels + x][buffer_pixels + tile_height_pixels + y] =
+                image_data[index];
+        }
+    }
+
+    if neighbor_tiles.right.is_some() {
+        let neighbor_tile = neighbor_tiles.right.as_ref().unwrap();
+        let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
+        let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
+
+        for index in 0..image_data.len() {
+            let x = index % neighbor_tile_width_pixels;
+
+            if x > buffer_pixels - 1 {
+                continue;
+            }
+
+            let y = index / neighbor_tile_width_pixels;
+            pixels[buffer_pixels + tile_width_pixels + x][buffer_pixels + y] = image_data[index];
+        }
+    }
+
+    if neighbor_tiles.bottom_right.is_some() {
+        let neighbor_tile = neighbor_tiles.bottom_right.as_ref().unwrap();
+        let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
+        let neighbor_tile_height_pixels = (neighbor_tile.max_y - neighbor_tile.min_y) as usize;
+        let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
+
+        for index in 0..image_data.len() {
+            let x = index % neighbor_tile_width_pixels;
+            let y = index / neighbor_tile_width_pixels;
+
+            if x > buffer_pixels - 1 || y < neighbor_tile_height_pixels - buffer_pixels {
+                continue;
+            }
+
+            pixels[buffer_pixels + tile_width_pixels + x]
+                [y + buffer_pixels - neighbor_tile_height_pixels] = image_data[index];
+        }
+    }
+
+    if neighbor_tiles.bottom.is_some() {
+        let neighbor_tile = neighbor_tiles.bottom.as_ref().unwrap();
         let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
         let neighbor_tile_height_pixels = (neighbor_tile.max_y - neighbor_tile.min_y) as usize;
         let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
@@ -167,53 +247,70 @@ fn get_pixels_with_buffer(
 
             let x = index % neighbor_tile_width_pixels;
 
-            pixels[x + buffer_pixels]
-                [buffer_pixels * 2 + tile_height_pixels + y - neighbor_tile_height_pixels] =
+            pixels[buffer_pixels + x][y + buffer_pixels - neighbor_tile_height_pixels] =
                 image_data[index];
         }
     }
 
-    if neighbor_tiles.top_right.is_some() {
-        let neighbor_tile = neighbor_tiles.top_right.unwrap();
-        // Because 1pixel = 1 metter
+    if neighbor_tiles.bottom_left.is_some() {
+        let neighbor_tile = neighbor_tiles.bottom_left.as_ref().unwrap();
         let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
         let neighbor_tile_height_pixels = (neighbor_tile.max_y - neighbor_tile.min_y) as usize;
         let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
 
         for index in 0..image_data.len() {
-            let x = index % usize::try_from(neighbor_tile_width_pixels).unwrap();
-            let y = index / usize::try_from(neighbor_tile_width_pixels).unwrap();
+            let x = index % neighbor_tile_width_pixels;
+            let y = index / neighbor_tile_width_pixels;
 
-            if y < neighbor_tile_height_pixels - buffer_pixels || x > buffer_pixels {
+            if x < neighbor_tile_width_pixels - buffer_pixels
+                || y < neighbor_tile_height_pixels - buffer_pixels
+            {
                 continue;
             }
 
-            pixels[buffer_pixels + tile_width_pixels + x]
-                [buffer_pixels * 2 + tile_height_pixels + y - neighbor_tile_height_pixels] =
-                image_data[index];
+            pixels[x + buffer_pixels - neighbor_tile_width_pixels]
+                [y + buffer_pixels - neighbor_tile_height_pixels] = image_data[index];
         }
     }
 
-    if neighbor_tiles.right.is_some() {
-        let neighbor_tile = neighbor_tiles.right.unwrap();
-        // Because 1pixel = 1 metter
+    if neighbor_tiles.left.is_some() {
+        let neighbor_tile = neighbor_tiles.left.as_ref().unwrap();
         let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
-        let neighbor_tile_height_pixels = (neighbor_tile.max_y - neighbor_tile.min_y) as usize;
         let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
 
         for index in 0..image_data.len() {
-            let x = index % usize::try_from(neighbor_tile_width_pixels).unwrap();
-            let y = index / usize::try_from(neighbor_tile_width_pixels).unwrap();
+            let x = index % neighbor_tile_width_pixels;
 
-            if x > buffer_pixels {
+            if x < neighbor_tile_width_pixels - buffer_pixels {
                 continue;
             }
 
-            pixels[buffer_pixels + tile_width_pixels + x]
-                [buffer_pixels * 2 + tile_height_pixels + y - neighbor_tile_height_pixels] =
+            let y = index / neighbor_tile_width_pixels;
+
+            pixels[x + buffer_pixels - neighbor_tile_width_pixels][buffer_pixels + y] =
                 image_data[index];
         }
     }
+
+    if neighbor_tiles.top_left.is_some() {
+        let neighbor_tile = neighbor_tiles.top_left.as_ref().unwrap();
+        let neighbor_tile_width_pixels = (neighbor_tile.max_x - neighbor_tile.min_x) as usize;
+        let image_data = get_image_data_from_tif(&neighbor_tile.dir_path.join(tif_filename));
+
+        for index in 0..image_data.len() {
+            let x = index % neighbor_tile_width_pixels;
+            let y = index / neighbor_tile_width_pixels;
+
+            if x < neighbor_tile_width_pixels - buffer_pixels || y > buffer_pixels - 1 {
+                continue;
+            }
+
+            pixels[x + buffer_pixels - neighbor_tile_width_pixels]
+                [buffer_pixels + tile_height_pixels + y] = image_data[index];
+        }
+    }
+
+    return pixels;
 }
 
 fn get_image_data_from_tif(path: &PathBuf) -> Vec<f64> {
